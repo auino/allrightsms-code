643f4fbeaeda8423c8e45c04f58a5d46b394c763
bruno.mazzarello@gmail.com <bruno.mazzarello@gmail.com@d3f1eefe-8cf5-cae9-1b2c-73b35515cf24>
2012-02-18 09:32:37 +0000

    MySuggestBox

---
+++ AllRightSMS-AppEngine/src/com/allrightsms/client/MySuggestBox.java
@@ -0,0 +1,51 @@
+package com.allrightsms.client;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.allrightsms.shared.ContactProxy;
+import com.allrightsms.shared.NumberUtility;
+import com.google.gwt.user.client.ui.MultiWordSuggestOracle;
+import com.google.gwt.user.client.ui.SuggestBox;
+
+public class MySuggestBox extends SuggestBox{
+
+	private String placeHolderText = "";
+	private final static MultiWordSuggestOracle mySuggestions = new MultiWordSuggestOracle();
+	private Map<String, String> map = new java.util.HashMap<String, String>();
+
+	
+	public MySuggestBox() {
+		super(mySuggestions);
+	}
+	
+	public String getPlaceHolderText() {
+		return placeHolderText;
+	}
+
+	public void setPlaceHolderText(String text) {
+		placeHolderText = text;
+		getTextBox().getElement().getStyle().setProperty("placeholder", placeHolderText);
+	}
+	
+	public void constructSuggestion(List<ContactProxy> contact)
+	{
+		map.clear();
+		for (ContactProxy c : contact) {
+			String temp = NumberUtility.createSuggestionString(c.getName(), c.getNumber());
+			map.put(c.getName(), c.getNumber());
+			mySuggestions.add(temp);
+		}
+	}
+	
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+}
+
+
